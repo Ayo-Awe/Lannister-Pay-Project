d9b9199f14298b9e347699fa21cfbb32a333dd49
Awe <pupoawe@icloud.com>
2022-07-08 21:22:24 +0100

    feat: setup POST endpoint to compute splitpayment

--- app.js
+++ app.js
@@ -2,8 +2,76 @@ const { request } = require("express");
 const express = require("express");
 const app = express();
 
-app.post("/", (req, res) => {
-  res.status(200).end();
+app.use(express.json());
+
+app.post("/split-payments/compute", (req, res) => {
+  const payload = req.body;
+  const { Amount, ID, SplitInfo } = payload;
+
+  let finalAmount = Amount;
+  let response = {
+    ID,
+    Balance: 0,
+    SplitBreakdown: [],
+  };
+
+  const flatSplit = SplitInfo.filter(
+    (e) => e.SplitType.toLowerCase() === "flat"
+  );
+
+  const percentageSplit = SplitInfo.filter(
+    (e) => e.SplitType.toLowerCase() === "percentage"
+  );
+
+  const ratioSplit = SplitInfo.filter(
+    (e) => e.SplitType.toLowerCase() == "ratio"
+  );
+
+  if (flatSplit.length > 0) {
+    // Compute flat calculations
+    flatSplit.forEach((element) => {
+      const { SplitEntityId, SplitValue } = element;
+
+      // Flat calculations have their amount equal to their splitvalue
+      finalAmount -= SplitValue;
+      response.SplitBreakdown.push({ SplitEntityId, Amount: SplitValue });
+    });
+  }
+
+  if (percentageSplit.length > 0) {
+    percentageSplit.forEach((element) => {
+      const { SplitEntityId, SplitValue } = element;
+
+      //Percentage calculations
+      const amount = (SplitValue * finalAmount) / 100;
+      finalAmount -= amount;
+
+      response.SplitBreakdown.push({ SplitEntityId, Amount: amount });
+    });
+  }
+
+  if (ratioSplit.length > 0) {
+    const openingRatioBalance = finalAmount;
+    const totalRatio = ratioSplit.reduce(ratioReducer, 0);
+
+    ratioSplit.forEach((element) => {
+      console.log("in ratio split");
+      const { SplitEntityId, SplitValue } = element;
+
+      //Ratio calculations
+      const amount = (openingRatioBalance * SplitValue) / totalRatio;
+      finalAmount -= amount;
+
+      response.SplitBreakdown.push({ SplitEntityId, Amount: amount });
+    });
+  }
+
+  response.Balance = finalAmount;
+  res.status(200).json(response);
 });
+
+const ratioReducer = (prev, curr) => {
+  return prev + curr.SplitValue;
+};
 
 module.exports = app;
