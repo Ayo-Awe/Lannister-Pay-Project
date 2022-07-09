5c45cde3aa9b98f8e85363cc9aa5177032432e33
Awe <pupoawe@icloud.com>
2022-07-09 23:53:51 +0100

    fix: load correct port from environment variables

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const app = require("./app");
-const port = process.env.PORT | 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log("listening for requests on port " + port);
 });
